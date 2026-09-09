f482f36165b071bfa7faeb05693bf47f210634c3
Sunsoer <suleymancho111@gmail.com>
2026-09-09 07:16:31 +0200

    Fix stable GitHub Pages base path

--- assets/clock-CTOwySGC.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./createServerFn-fM_x-ST8.js";import{a as t}from"./index-DkpGcw0j.js";var n=e({method:`GET`}).handler(t(`8aa19ce0c573bc4202bc9b8b72568cdff4ce64a852d1d279872689bd0dffb5c7`));export{n as getDailyClock};
