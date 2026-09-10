6c33828fbf286075df9cc4f0bf608d2c57b78c89
Sunsoer <suleymancho111@gmail.com>
2026-09-10 19:04:47 +0200

    Fix GitHub Pages PWA base path

---
+++ assets/clock-DlJFts0M.js
@@ -0,0 +1,1 @@
+import{t as e}from"./createServerFn-DKT_UIvK.js";import{a as t}from"./index-CL2DueeY.js";var n=e({method:`GET`}).handler(t(`8aa19ce0c573bc4202bc9b8b72568cdff4ce64a852d1d279872689bd0dffb5c7`));export{n as getDailyClock};
